7d6a8ba4fd4a223b830e51d55cba59f9c42a0124
souvik <souvik@aspire>
2019-05-01 09:00:19 +0530

    Initial Commit

---
+++ lib-quiz-entity/src/main/java/com/proj/auth/entity/LibQuizEntity.java
@@ -0,0 +1,13 @@
+package com.proj.auth.entity;
+
+/**
+ * Hello world!
+ *
+ */
+public class LibQuizEntity 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lib-quiz-entity/src/test/java/com/proj/auth/entity/LibQuizEntityTest.java
@@ -0,0 +1,38 @@
+package com.proj.auth.entity;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class LibQuizEntityTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public LibQuizEntityTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( LibQuizEntityTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
